2d5befba6e53e5d818f16377c24fc50e0189cdad
Elton <elton.b.braga@gmail.com>
2025-07-01 14:03:43 -0300

    criadas as telas de login e lista

--- INCRA/src/app/app.routes.ts
+++ INCRA/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ListaComponent } from './lista/lista.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'lista',
+    component: ListaComponent,
   },
 ];
--- INCRA/src/app/login/login.component.ts
+++ INCRA/src/app/login/login.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  email: string = '';
+  senha: string = '';
 
+  ValidaFofrm = new FormControl({
+    email: new FormControl(''),
+    senha: new FormControl(''),
+  });
+
+  ngOnInit() {
+    this.ValidaFofrm;
+  }
 }
