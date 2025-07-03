db4d25fb709d6127c3d0d03a840187206c443f34
Elton <elton.b.braga@gmail.com>
2025-07-03 17:42:39 -0300

    criados os campos de busca e data table

--- INCRA/src/app/lista/lista.component.ts
+++ INCRA/src/app/lista/lista.component.ts
@@ -4,6 +4,18 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-lista',
@@ -13,46 +25,211 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
     MatInputModule,
     MatSortModule,
     MatPaginatorModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss',
 })
 export class ListaComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
+  displayedColumns: string[] = [
+    'beneficiario',
+    'data_transferencia',
+    'codigo',
+    'nome_projeto',
+    'acoes',
+  ];
+  dataSource = new MatTableDataSource<Beneficiario>(MOCK_BENEFICIARIOS);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  form!: FormGroup;
+  codigo_beneficiario: any;
+  codigo_peojeto: any;
+  cpf: any;
+  ordem: any;
 
+  toppings = new FormControl('');
+  tipo_busca: string[] = ['Nome do Beneficiario', 'Codigo do Beneficiario'];
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      codigo_beneficiario: [],
+      codigo_projeto: [],
+      cpf: [],
+      ordem: [],
+      tipo_busca: [],
+    });
+  }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
+
+  formatarData(dataNumerica: number): string {
+    const str = dataNumerica.toString();
+    const ano = +str.substring(0, 4);
+    const mes = +str.substring(4, 6) - 1;
+    const dia = +str.substring(6, 8);
+    const data = new Date(ano, mes, dia);
+    return new Intl.DateTimeFormat('pt-BR').format(data);
+  }
+
+  executarAcao(acao: string, elemento: Beneficiario) {
+    console.log(`Ação "${acao}" executada para CPF ${elemento.beneficiario}`);
+    // Aqui você pode redirecionar, abrir modal, etc.
+  }
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+/*area de mock */
+
+export interface Beneficiario {
+  beneficiario: string;
+  data_transferencia: number;
+  codigo: number;
+  nome_projeto: string;
+  acoes: any;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+
+export const MOCK_BENEFICIARIOS: Beneficiario[] = [
+  {
+    beneficiario: '12345678900',
+    data_transferencia: 20240115,
+    codigo: 101,
+    nome_projeto: 'Projeto Esperança',
+    acoes: ['visualizar', 'editar'],
+  },
+  {
+    beneficiario: '23456789011',
+    data_transferencia: 20240210,
+    codigo: 102,
+    nome_projeto: 'Projeto Liberdade',
+    acoes: ['visualizar'],
+  },
+  {
+    beneficiario: '34567890122',
+    data_transferencia: 20240305,
+    codigo: 103,
+    nome_projeto: 'Projeto Vida Nova',
+    acoes: ['editar'],
+  },
+  {
+    beneficiario: '45678901233',
+    data_transferencia: 20240401,
+    codigo: 104,
+    nome_projeto: 'Projeto Cidadania',
+    acoes: ['visualizar', 'remover'],
+  },
+  {
+    beneficiario: '56789012344',
+    data_transferencia: 20240510,
+    codigo: 105,
+    nome_projeto: 'Projeto Esperança',
+    acoes: ['visualizar', 'editar', 'remover'],
+  },
+  {
+    beneficiario: '67890123455',
+    data_transferencia: 20240612,
+    codigo: 106,
+    nome_projeto: 'Projeto Sementes',
+    acoes: ['visualizar'],
+  },
+  {
+    beneficiario: '78901234566',
+    data_transferencia: 20240720,
+    codigo: 107,
+    nome_projeto: 'Projeto Terra Livre',
+    acoes: ['editar'],
+  },
+  {
+    beneficiario: '89012345677',
+    data_transferencia: 20240830,
+    codigo: 108,
+    nome_projeto: 'Projeto Raízes',
+    acoes: ['remover'],
+  },
+  {
+    beneficiario: '90123456788',
+    data_transferencia: 20240901,
+    codigo: 109,
+    nome_projeto: 'Projeto Água Doce',
+    acoes: ['visualizar', 'editar'],
+  },
+  {
+    beneficiario: '01234567899',
+    data_transferencia: 20241015,
+    codigo: 110,
+    nome_projeto: 'Projeto Recomeço',
+    acoes: ['visualizar'],
+  },
+  {
+    beneficiario: '11223344556',
+    data_transferencia: 20240120,
+    codigo: 111,
+    nome_projeto: 'Projeto Horizonte',
+    acoes: ['visualizar', 'remover'],
+  },
+  {
+    beneficiario: '22334455667',
+    data_transferencia: 20240225,
+    codigo: 112,
+    nome_projeto: 'Projeto Semente Viva',
+    acoes: ['editar'],
+  },
+  {
+    beneficiario: '33445566778',
+    data_transferencia: 20240330,
+    codigo: 113,
+    nome_projeto: 'Projeto Novo Caminho',
+    acoes: ['visualizar'],
+  },
+  {
+    beneficiario: '44556677889',
+    data_transferencia: 20240412,
+    codigo: 114,
+    nome_projeto: 'Projeto Brasil Verde',
+    acoes: ['editar', 'remover'],
+  },
+  {
+    beneficiario: '55667788990',
+    data_transferencia: 20240518,
+    codigo: 115,
+    nome_projeto: 'Projeto Vale Rural',
+    acoes: ['visualizar', 'editar'],
+  },
+  {
+    beneficiario: '66778899001',
+    data_transferencia: 20240622,
+    codigo: 116,
+    nome_projeto: 'Projeto Esperança',
+    acoes: ['remover'],
+  },
+  {
+    beneficiario: '77889900112',
+    data_transferencia: 20240728,
+    codigo: 117,
+    nome_projeto: 'Projeto Liberdade',
+    acoes: ['visualizar'],
+  },
+  {
+    beneficiario: '88990011223',
+    data_transferencia: 20240831,
+    codigo: 118,
+    nome_projeto: 'Projeto Cidadania',
+    acoes: ['editar'],
+  },
+  {
+    beneficiario: '99001122334',
+    data_transferencia: 20240905,
+    codigo: 119,
+    nome_projeto: 'Projeto Raízes',
+    acoes: ['visualizar', 'editar'],
+  },
+  {
+    beneficiario: '10011223345',
+    data_transferencia: 20241020,
+    codigo: 120,
+    nome_projeto: 'Projeto Terra Livre',
+    acoes: ['remover'],
+  },
 ];
--- INCRA/src/app/login/login.component.ts
+++ INCRA/src/app/login/login.component.ts
@@ -25,8 +25,21 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 })
 export class LoginComponent {
   registrationForm: FormGroup;
-  cpf: string = '';
-  senha: string = '';
+  cpf: any = '';
+  senha: any = '';
+  mockUsuarios = [
+    { cpf: '111.111.111-11', senha: '123456' },
+    { cpf: '222.222.222-22', senha: 'senha123' },
+    { cpf: '333.333.333-33', senha: 'abc123' },
+    { cpf: '444.444.444-44', senha: 'qwerty' },
+    { cpf: '555.555.555-55', senha: 'admin' },
+    { cpf: '666.666.666-66', senha: 'root123' },
+    { cpf: '777.777.777-77', senha: 'senha777' },
+    { cpf: '888.888.888-88', senha: 'pass888' },
+    { cpf: '999.999.999-99', senha: 'senha999' },
+    { cpf: '000.000.000-00', senha: 'zerozero' },
+  ];
+  erroAutenticacao = '';
 
   constructor(private fb: FormBuilder) {
     this.registrationForm = this.fb.group({
@@ -35,36 +48,31 @@ export class LoginComponent {
     });
   }
 
-  onSubmit(): void {
-    if (this.registrationForm.valid) {
-      console.log('Form Data:', this.registrationForm.value);
-    } else {
-      console.log('Form is invalid.');
-    }
-  }
-
   ngOnInit() {}
-
-  get cpfUser(): AbstractControl {
+  //Garante que o campo não seja nulo, se foi tocado ou se houve alguma outra ação
+  get cpfTouched(): AbstractControl {
     return this.registrationForm.get('cpf')!;
   }
 
-  get senhaUser(): AbstractControl {
+  get senhaTouched(): AbstractControl {
     return this.registrationForm.get('senha')!;
   }
+  onSubmit(): void {
+    this.cpf.value;
+    const senha = this.senha.value;
+
+    //ealiza a busca de um usuário com CPF correspondente na lista de usuários fictícios (mockUsuarios)
+    const usuario = this.mockUsuarios.find((u) => u.cpf === this.cpf.value);
+
+    if (!usuario) {
+      this.erroAutenticacao = 'CPF não encontrado.';
+    } else if (usuario.senha !== this.senha.value) {
+      console.log((this.erroAutenticacao = 'Senha incorreta.'));
+    } else {
+      this.erroAutenticacao = '';
+      console.log('Login realizado com sucesso!');
+    }
+
+    console.log(this.registrationForm.value);
+  }
 }
-
-const erroAutenticacao = '';
-
-const mockUsuarios = [
-  { cpf: '111.111.111-11', senha: '123456' },
-  { cpf: '222.222.222-22', senha: 'senha123' },
-  { cpf: '333.333.333-33', senha: 'abc123' },
-  { cpf: '444.444.444-44', senha: 'qwerty' },
-  { cpf: '555.555.555-55', senha: 'admin' },
-  { cpf: '666.666.666-66', senha: 'root123' },
-  { cpf: '777.777.777-77', senha: 'senha777' },
-  { cpf: '888.888.888-88', senha: 'pass888' },
-  { cpf: '999.999.999-99', senha: 'senha999' },
-  { cpf: '000.000.000-00', senha: 'zerozero' },
-];
