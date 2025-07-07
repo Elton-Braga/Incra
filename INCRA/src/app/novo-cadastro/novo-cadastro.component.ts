306157d0456336fab76cba5512d4d2c85f7a217c
Elton <elton.b.braga@gmail.com>
2025-07-07 17:03:50 -0300

    Breadcramb ajustado

--- INCRA/src/app/novo-cadastro/novo-cadastro.component.ts
+++ INCRA/src/app/novo-cadastro/novo-cadastro.component.ts
@@ -1,30 +1,75 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-novo-cadastro',
-  imports: [MatButtonModule, RouterLink, BreadcrumbModule, RouterOutlet],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    BreadcrumbModule,
+    RouterOutlet,
+    NgIf,
+    MatIconModule,
+  ],
   templateUrl: './novo-cadastro.component.html',
   styleUrl: './novo-cadastro.component.scss',
 })
 export class NovoCadastroComponent {
-  item: string = '';
-  items: any[] | undefined;
+  items: any[] = [];
+  home = { icon: 'pi pi-home', routerLink: '/inicio' };
 
-  home: any | undefined;
+  etapas = ['tela1'];
+  etapaAtualIndex = 0;
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.items = [
-      { label: 'Tela 1' },
-      { label: 'Tela 2' },
-      { label: 'Tela 3' },
-      { label: 'Tela 4' },
-      { label: 'Tela 5' },
-    ];
+    this.atualizarBreadcrumb();
 
-    this.home = { icon: 'pi pi-home', routerLink: '/' };
+    this.route.firstChild?.url.subscribe((urlSegment) => {
+      const etapa = urlSegment[0]?.path;
+      this.etapaAtualIndex = this.etapas.indexOf(etapa);
+      this.atualizarBreadcrumb();
+    });
+  }
+
+  atualizarBreadcrumb() {
+    this.items = this.etapas
+      .slice(0, this.etapaAtualIndex + 1)
+      .map((etapa) => ({
+        label: this.capitalizar(etapa),
+      }));
+  }
+
+  capitalizar(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  voltar() {
+    if (this.etapaAtualIndex > 0) {
+      const rota = this.etapas[this.etapaAtualIndex - 1];
+      this.router.navigate(['../' + rota], { relativeTo: this.route });
+    }
+  }
+
+  avancar() {
+    if (this.etapaAtualIndex < this.etapas.length - 1) {
+      const rota = this.etapas[this.etapaAtualIndex + 1];
+      this.router.navigate(['../' + rota], { relativeTo: this.route });
+    }
+  }
+
+  ehUltimaEtapa(): boolean {
+    return this.etapaAtualIndex === this.etapas.length - 1;
+  }
+
+  salvar() {
+    console.log('Dados salvos!');
+    // Aqui poderá chamar serviço de API, exibir snackbar etc.
   }
 }
---
+++ INCRA/src/app/novo-cadastro/tela-1/tela-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-1',
+  imports: [],
+  templateUrl: './tela-1.component.html',
+  styleUrl: './tela-1.component.scss'
+})
+export class Tela1Component {
+
+}
