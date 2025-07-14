3353f958ab5a820d16cce389c60e42756afd2a84
Elton <elton.b.braga@gmail.com>
2025-07-14 15:15:42 -0300

    criado o componente assentamento e ajustado o botão do coponente unidade familiar relacioando ao campo associação unidade familiar

---
+++ INCRA/src/app/novo-cadastro/assentamento/assentamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assentamento',
+  imports: [],
+  templateUrl: './assentamento.component.html',
+  styleUrl: './assentamento.component.scss'
+})
+export class AssentamentoComponent {
+
+}
